38f499b295a549ca72c4772b94be132464dbd4b0
hafizapatel04-bit <hafizapatel04@gmail.com>
2026-09-06 23:35:11 +0530

    Time: 7 ms (42.82%), Space: 13.2 MB (68.14%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+
+        n = len(nums)
+        ans = n
+
+        for i in range (n):
+            ans ^= i
+            ans ^= nums[i]
+
+        return ans
